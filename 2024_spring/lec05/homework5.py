cfcfef9e21d0da1c585c80392deb72bdcf7e37b6
M23W0014 <166461461+M23W0014@users.noreply.github.com>
2024-05-21 09:41:50 +0900

    update homework

--- 2024_spring/lec05/homework5.py
+++ 2024_spring/lec05/homework5.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,11 +17,13 @@ def center_of_gravity(x):
     @result:
     c (scalar): x's center of gravity
     '''
-    c = 0  # change this line
+    N = len(x)
+    n = np.arange(N)
+    c = np.dot(n,x)/np.sum(x)
     return c
 
 def matched_identity(x):
-    '''
+    ''' 
     Create an identity matrix that has the same number of rows as x has elements.
     Hint: use len(x), and use np.eye.
     
@@ -30,7 +33,9 @@ def matched_identity(x):
     @result:
     I (array): a 2d numpy array: an NxN identity matrix
     '''
-    I =  0 # change this line
+    N = len(x)
+    I = np.eye(N)
+    
     return I
 
 def sine_and_cosine(t_start, t_end, t_steps):
@@ -49,8 +54,8 @@ def sine_and_cosine(t_start, t_end, t_steps):
     y (array of length t_steps): sin(t)
     '''
     # change these lines
-    t = 0 
-    x = 0
-    y = 0
+    t = np.linspace(t_start,t_end,t_steps)
+    x = np.cos(t)
+    y = np.sin(t)
     # end changes here
     return t, x, y
